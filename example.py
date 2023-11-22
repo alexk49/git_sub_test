adecff2e51f94c90042a80244e16d041f8d9be0d
alexk49 <alexcode9@mailbox.org>
2023-11-22 08:46:05 +0000

    first commit example

--- example.py
+++ example.py
@@ -1,2 +1,11 @@
+from git_test import *
+
 def add_numbers(number, another_number):
     return (number + another_number)
+
+def main():
+    git_test.real-code.dog()
+    
+
+if __name__ == "__main__":
+    main()
