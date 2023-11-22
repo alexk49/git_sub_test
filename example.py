0a2414b067c7c8080aeb26a9e39787ac90bc3949
alexk49 <alexcode9@mailbox.org>
2023-11-22 08:49:52 +0000

    corrected imports

--- example.py
+++ example.py
@@ -1,11 +1,15 @@
-from git_test import *
+from git_test.real_code import hello_world
+
 
 def add_numbers(number, another_number):
-    return (number + another_number)
+    return number + another_number
+
 
 def main():
-    git_test.real-code.dog()
-    
+    hello_world()
+
+    print(add_numbers(1, 2))
+
 
 if __name__ == "__main__":
     main()
